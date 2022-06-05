9548ba9c8a264c921504c45a4e6613afefc6dbf3
Lucas4d4 <101574260+Lucas4d4@users.noreply.github.com>
2022-06-05 17:29:55 -0300

    Agregado las partes Private y Public de la clase

--- Entidades/Sucursal.cs
+++ Entidades/Sucursal.cs
@@ -1,9 +1,44 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Entidades
 {
     public class Sucursal
     {
+        // PARTE PRIVATE
+        private int id_Sucursal;
+        private String NombreSucursal;
+        private String DescripcionSucursal;
+        private int id_HorarioSucursal;
+        private int id_ProvinciaSucursal;
+        private String DireccionSucursal;
+        private String URL_DireccionSucursal;
+        private String DescripcionProvincia_Sucursal;
+        public Sucursal()
+        {
+
+        }
+        // PARTE PUBLIC
+        //PARTE sets()
+        public void setid_Sucursal(int isu) { id_Sucursal = isu; }
+        public void setNombreSucursal(String ns) { NombreSucursal = ns; }
+        public void setDescripcionSucursal(String ds) { DescripcionSucursal = ds; }
+        public void setid_HorarioSucursal(int hs) { id_HorarioSucursal = hs; }
+        public void setid_ProvinciaSucursal(int ips) { id_ProvinciaSucursal = ips; }
+        public void setDireccionSucursal(String dirs) { DireccionSucursal = dirs; }
+        public void setURL_DireccionSucursal(String uds) { URL_DireccionSucursal = uds; }
+        public void setDescripcionProvincia_Sucursal(String dps) { DescripcionProvincia_Sucursal = dps; }
+        //PARTE gets()
+        public int getid_Sucursal() { return id_Sucursal; }
+        public String getNombreSucursal() { return NombreSucursal; }
+        public String getDescripcionSucursal() { return DescripcionSucursal; }
+        public int getid_HorarioSucursal() { return id_HorarioSucursal; }
+        public int getid_ProvinciaSucursal() { return id_ProvinciaSucursal; }
+        public String getDireccionSucursal() { return DireccionSucursal; }
+        public String getURL_DireccionSucursal() { return URL_DireccionSucursal; }
+        public String getDescripcionProvincia_Sucursal() { return DescripcionProvincia_Sucursal; }
     }
 }
