f07f15639924dedf6871d24d07993d8ac0fe764a
Lucas4d4 <101574260+Lucas4d4@users.noreply.github.com>
2022-06-06 11:55:30 -0300

    Nueva clase DaoSucursal

---
+++ DAO/DaoSucursal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAO
+{
+    public static class DaoSucursal
+    {
+       
+         public static DataTable getTablaSucursal()
+        {
+        DataTable tabla = DB.ObtenerTabla("Sucursal", "Select Id_Sucursal, NombreSucursal, DescripcionSucursal, DescripcionProvincia, DireccionSucursal from Sucursal INNER JOIN Provincia ON Sucursal.Id_ProvinciaSucursal=Provincia.Id_Provincia");
+        return tabla;
+        }
+
+        public static DataTable getTablaSucursalIndividual(String id)
+        {
+            DataTable tabla = DB.ObtenerTabla("Sucursal", "Select Id_Sucursal, NombreSucursal, DescripcionSucursal, DescripcionProvincia, DireccionSucursal from Sucursal INNER JOIN Provincia ON Sucursal.Id_ProvinciaSucursal=Provincia.Id_Provincia WHERE Id_Sucursal="+id);
+            return tabla;
+        }
+    }
+}
